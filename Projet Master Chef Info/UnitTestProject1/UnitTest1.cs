aa0251810ac9bc4fbc2c7ac504f22c2ce755abfe
tejeshhhh <tejesh.flgs@gmail.com>
2018-12-07 10:27:35 +0100

    Updates

---
+++ Projet Master Chef Info/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Projet Master Chef Info/UnitTestProject2/BDDConnectorTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Tests
+{
+    [TestClass()]
+    public class BDDConnectorTests
+    {
+        [TestMethod()]
+        public void GetAllStockTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteFromStockTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddFromStockTest()
+        {
+
+            bool result = SqlCheck(username, password);
+            if (result)
+            {
+
+
+            }
+            else
+            {
+
+
+            }
+        }
+
+
+    }
+}
