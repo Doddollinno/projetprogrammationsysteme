1967b602bea19e2d016f60273d32e679a738e7ab
tejeshhhh <tejesh.flgs@gmail.com>
2018-12-11 17:16:34 +0100

    Updates

--- Projet Master Chef Info/UnitTestProject2/BDDConnectorTests.cs
+++ Projet Master Chef Info/UnitTestProject2/BDDConnectorTests.cs
@@ -16,39 +16,21 @@ namespace Model.Tests
         {
             Assert.Fail();
         }
-        /*Utiliser Assert.Fail() avec la doc présent sur stack overflow et trouver un moyen de récupérer la liste des ingrédients
-         dans le stock, et l'afficher, je n'ai pas encore trouver de solution pour cela*/
+        
 
         [TestMethod()]
         public void DeleteFromStockTest()
         {
-            int a = 5;
-            int b = 1;
-            int c = a - b;
-            Console.WriteLine(c);
+           
+            
 
-            int d = 14;
-            int e > 2;
-            int f = d - e;
-            Console.WriteLine(f);
         }
 
 
         [TestMethod()]
         public void AddFromStockTest()
         {
-            int a = 7;
-            int b = 1;
-            int c = a + b;
-            Console.WriteLine(c);
-
-            int d = 14;
-            int e > 2;
-            int f = d + e;
-            Console.WriteLine(f);
-            /*Une fois que les tests sont passés, il faut assosier les bons noms, pour ajouter ou supprimer des ingrédients 
-             de la base de donné il faudra effectué un test quand la base de donnée sera remplie et connectée. Les tests devraient être
-            validée*/
+            
            
 
         }
